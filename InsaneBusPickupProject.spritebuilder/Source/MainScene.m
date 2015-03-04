00e494b737538490755346b621e082a1170d48b4
Yao Fan <Frank@Yaos-MacBook-Pro.local>
2015-03-03 22:57:51 -0800

    done by Frank

--- InsaneBusPickupProject.spritebuilder/Source/MainScene.m
+++ InsaneBusPickupProject.spritebuilder/Source/MainScene.m
@@ -4,8 +4,8 @@
 
 //Comment added by Rahul Thankachan
 //Please add ur comment below and Push
-    
-    //comment added by Yao "Frank" Fan
+//comment by stephen
+    // added by Yao "Frank" Fan
 }
 
 @end
